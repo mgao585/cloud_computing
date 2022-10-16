01868920b55e5ded6f595c4097a3926f41fdbc2a
mgao585 <mgao585@gmail.com>
2022-10-16 00:49:23 -0700

    lab4

---
+++ lab4/api/code/api.py
@@ -0,0 +1,33 @@
+from flask import Flask, request 
+import random
+import json
+app = Flask(__name__)
+
+@app.route('/')
+def meal_pick():
+    meals = [
+    {'Meal': 'Meal A',
+     'Price': '$1'},
+    {'Meal': 'Meal B',
+     'Price': '$2'},
+    {'Meal': 'Meal C',
+     'Price': '$3'},
+    {'Meal': 'Meal D',
+     'Price': '$4'},
+    {'Meal': 'Meal E',
+     'Price': '$5'},
+    {'Meal': 'Meal F',
+     'Price': '$6'},
+    {'Meal': 'Meal G',
+     'Price': '$7'},
+    {'Meal': 'Meal H',
+     'Price': '$8'},
+    {'Meal': 'Meal I',
+     'Price': '$9'},
+    {'Meal': 'Meal J',
+     'Price': '$10'},]
+    meal_picked = random.choice(meals)
+    return json.dumps(meal_picked)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
