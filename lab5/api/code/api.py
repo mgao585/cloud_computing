e6991fe19d86f4a050ba3bf4cfb41e3d08d6e0fa
mgao585 <mgao585@gmail.com>
2022-10-20 20:05:55 -0700

    lab5

---
+++ lab5/api/code/api.py
@@ -0,0 +1,31 @@
+from flask import Flask, request 
+# import random
+import json
+from sqlalchemy import create_engine
+import os
+app = Flask(__name__)
+
+db_name = os.environ.get('DB_NAME')
+db_user = os.environ.get('PG_USER')
+db_pass = os.environ.get('PG_PASS')
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('PG_PORT') 
+
+url = f'postgresql://{db_user}:{db_pass}@{db_host}:{str(db_port)}/{db_name}'
+# url = 'postgresql://' + db_user + ':'+ db_pass + '@'+ db_host + ':' + str(db_port) + '/' + db_name
+db = create_engine(url)
+
+@app.route('/meal_rec')
+@app.route('/')
+def meal_pick():
+      query = 'SELECT Meal_Name, Meal_Price FROM meal ORDER BY RANDOM() LIMIT 1;'
+      row = db.execute(query)
+      meal_picked = {}
+      for r in row:
+            meal_picked['Meal'] = r[0]
+            meal_picked['Price'] = r[1]
+      
+      return json.dumps(meal_picked)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=os.environ.get("API_PORT"), debug=True)
---
+++ lab5/db/sql/init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS meal (
+    Meal_ID SERIAL PRIMARY KEY,
+    Meal_Name TEXT NOT NULL,
+    Meal_Price INTEGER NOT NULL
+);
+
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal1', 1);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal2', 2);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal3', 3);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal4', 4);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal5', 5);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal6', 6);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal7', 7);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal8', 8);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal9', 9);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal10', 10);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal11', 11);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal12', 12);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal13', 13);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal14', 14);
+INSERT INTO meal(Meal_Name, Meal_Price) VALUES ('Meal15', 15);
